f6937318526666e8d0ba7dcde034f728921d0dcd
wandriey <jona434g@edu.easj.dk>
2018-04-02 14:11:46 +0200

    Initial commit

--- ConsoleTestAfSoapWebService/Program.cs
+++ ConsoleTestAfSoapWebService/Program.cs
@@ -10,6 +10,19 @@ namespace ConsoleTestAfSoapWebService
     {
         static void Main(string[] args)
         {
+            //Først oprettet vi en "service referance" til WSDL filen på vores SOAP webservice. 
+            //dereftrer laver vi en variable der peger på webservicen, så nu kan vi via. variablen
+            //kalde alle de metoder og propperties der er i denne. 
+            using (var client = new ServiceReference1.ServiceClient("BasicHttpBinding_IService"))
+            {
+                //getMyEmail metode fra vores webservice 
+                Console.WriteLine(client.getMyEmail()); 
+
+                //OBS! SOAP webservicen skal køre, eller kommer den med en fejl 
+            }
+
+            Console.ReadLine( );
+
         }
     }
 }
